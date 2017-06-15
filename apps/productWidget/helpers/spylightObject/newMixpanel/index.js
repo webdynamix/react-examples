83ed0d9be3f1c1353997dad2cba1c8593062f65c
webdynamix <vale@webdynamix.com>
2017-06-14 21:24:14 -0700

    Create index.js

--- apps/productWidget/helpers/spylightObject/newMixpanel/index.js
+++ apps/productWidget/helpers/spylightObject/newMixpanel/index.js
@@ -8,7 +8,7 @@ function mixpanelSetup() {
   const models = ['outfitId', 'publisherId', 'sessionIdentifier', 'referralUrl'];
 
   const newMixpanel = new Mixpanel({
-    token: '8e02e4feef228ce5b4510598cc7344d0',
+    token: 'TOKEN',
     models: {
       error: concat(models, ['message']),
       closeModal: concat(models, ['manualClose']),
